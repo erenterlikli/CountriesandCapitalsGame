d38bdb2992c54633b63c88b6338652b2662c3d11
eren.terlikli <44238447+erenterlikli@users.noreply.github.com>
2020-10-24 18:17:26 +0300

    Add files via upload

---
+++ ÜlkelerveBaşkentleri/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace ÜlkelerveBaşkentleri
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Location = new System.Drawing.Point(113, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(503, 196);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(46, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(113, 267);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 42);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(462, 267);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(154, 42);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(113, 337);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(154, 42);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(462, 337);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(154, 42);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(291, 396);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(154, 42);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button6.Location = new System.Drawing.Point(634, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(154, 42);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Başlat";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(335, 225);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 25);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "-";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(706, 246);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "0";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(706, 300);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "0";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(656, 108);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 25);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Puan:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(750, 108);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(24, 25);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(750, 150);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(24, 25);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(656, 150);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(65, 25);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Süre:";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Ülkeler ve Başkentleri";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ ÜlkelerveBaşkentleri/Form1.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ÜlkelerveBaşkentleri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int sayac = 0;
+        int puan = 0;
+        int sure = 60;
+
+        private void ButonSıfırla()
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            button6.Enabled = true;
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            sure--;
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+            button6.Enabled = false;
+            button1.BackColor = DefaultBackColor;
+            button2.BackColor = DefaultBackColor;
+            button3.BackColor = DefaultBackColor;
+            button4.BackColor = DefaultBackColor;
+            button5.BackColor = DefaultBackColor;
+            button6.Text = "Sonraki Soru";
+            sayac++;
+            label1.Text = sayac.ToString();
+
+            if(sayac==1)
+            {
+                label2.Text = "Fransa";
+                pictureBox1.ImageLocation = @"C:\Users\Eren\source\repos\ÜlkelerveBaşkentleri\Yeni klasör\paris.jpg";
+                button1.Text = "Lille";
+                button2.Text = "Paris";
+                button3.Text = "Marsilya";
+                button4.Text = "Lyon";
+                button5.Text = "Bordeaux";
+                label3.Text = "Paris";
+            }
+
+            if(sayac==2)
+            {
+                label2.Text = "Almanya";
+                pictureBox1.ImageLocation = @"C:\Users\Eren\source\repos\ÜlkelerveBaşkentleri\Yeni klasör\berlin.jpg";
+                button1.Text = "Hamburg";
+                button2.Text = "Hannover";
+                button3.Text = "Berlin";
+                button4.Text = "Frankfurt";
+                button5.Text = "Kiel";
+                label3.Text = "Berlin";
+            }
+
+            if(sayac==3)
+            {
+                label2.Text = "İngiltere";
+                pictureBox1.ImageLocation= @"C:\Users\Eren\source\repos\ÜlkelerveBaşkentleri\Yeni klasör\londra.jpg";
+                button1.Text = "Londra";
+                button2.Text = "Manchester";
+                button3.Text = "Cambridge";
+                button4.Text = "Oxford";
+                button5.Text = "Bristol";
+                label3.Text = "Londra";
+            }
+
+            if(sayac==4)
+            {
+                label2.Text = "İtalya";
+                pictureBox1.ImageLocation= @"C:\Users\Eren\source\repos\ÜlkelerveBaşkentleri\Yeni klasör\roma.jpg";
+                button1.Text = "Venedik";
+                button2.Text = "Milano";
+                button3.Text = "Floransa";
+                button4.Text = "Roma";
+                button5.Text = "Napoli";
+                label3.Text = "Roma";
+            }
+
+            if(sayac==5)
+            {
+                label2.Text = "Türkiye";
+                pictureBox1.ImageLocation = @"C:\Users\Eren\source\repos\ÜlkelerveBaşkentleri\Yeni klasör\ankara.jpg";
+                button1.Text = "İstanbul";
+                button2.Text = "Ankara";
+                button3.Text = "İzmir";
+                button4.Text = "Bursa";
+                button5.Text = "Adana";
+                label3.Text = "Ankara";
+            }
+
+            if(sayac==6)
+            {
+                label2.Text = "Hollanda";
+                pictureBox1.ImageLocation = @"C:\Users\Eren\source\repos\ÜlkelerveBaşkentleri\Yeni klasör\amsterdam.jpg";
+                button1.Text = "Amsterdam";
+                button2.Text = "Rotterdam";
+                button3.Text = "Lahey";
+                button4.Text = "Eindhoven";
+                button5.Text = "Delft";
+                label3.Text = "Amsterdam";
+            }
+
+            if(sayac == 7)
+            {
+                label2.Text = "İsveç";
+                pictureBox1.ImageLocation = @"C:\Users\Eren\source\repos\ÜlkelerveBaşkentleri\Yeni klasör\stockholm.jpg";
+                button1.Text = "Göteborg";
+                button2.Text = "Helsinborg";
+                button3.Text = "Stockholm";
+                button4.Text = "Boras";
+                button5.Text = "Malmö";
+                label3.Text = "Stockholm";
+            }
+
+            if(sayac == 8)
+            {
+                label2.Text = "Kanada";
+                pictureBox1.ImageLocation = @"C:\Users\Eren\source\repos\ÜlkelerveBaşkentleri\Yeni klasör\ottawa.jpg";
+                button1.Text = "Toronto";
+                button2.Text = "Vancouver";
+                button3.Text = "Montreal";
+                button4.Text = "Edmonton";
+                button5.Text = "Ottawa";
+                label3.Text = "Ottawa";
+            }
+
+            if(sayac == 9)
+            {
+                label2.Text = "İsviçre";
+                pictureBox1.ImageLocation = @"C:\Users\Eren\source\repos\ÜlkelerveBaşkentleri\Yeni klasör\bern.jpg";
+                button1.Text = "Cenevre";
+                button2.Text = "Bern";
+                button3.Text = "Zürih";
+                button4.Text = "Lozan";
+                button5.Text = "Montrö";
+                label3.Text = "Bern";
+            }
+
+            if(sayac == 10)
+            {
+                label2.Text = "Bosna Hersek";
+                pictureBox1.ImageLocation = @"C:\Users\Eren\source\repos\ÜlkelerveBaşkentleri\Yeni klasör\saraybosna.jpg";
+                button1.Text = "Saraybosna";
+                button2.Text = "Mostar";
+                button3.Text = "Travnik";
+                button4.Text = "Tuzla";
+                button5.Text = "Banja Luka";
+                label3.Text = "Saraybosna";
+            }
+
+            if(sayac==11)
+            {
+                timer1.Stop();
+                label1.Text = "0";
+                pictureBox1.ImageLocation = " ";
+                button1.Text = " ";
+                button2.Text = " ";
+                button3.Text = " ";
+                button4.Text = " ";
+                button5.Text = " ";
+                label2.Text = " ";
+                puan = puan + Convert.ToInt32(label7.Text);
+                label6.Text = puan.ToString();
+                MessageBox.Show("Tebrikler,oyun bitti. Puanınız:" + label6.Text);
+                Application.Exit();
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label4.Text = button1.Text;
+            ButonSıfırla();
+           
+           
+            if(label3.Text==label4.Text)
+            {
+                puan = puan + 10;
+                label6.Text = puan.ToString();
+                button1.BackColor = Color.Green;
+            }
+
+            else
+            {
+                puan = puan - 5;
+                label6.Text = puan.ToString();
+                button1.BackColor = Color.Red;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label4.Text = button2.Text;
+            ButonSıfırla();
+            if(label3.Text==label4.Text)
+            {
+                puan = puan + 10;
+                label6.Text = puan.ToString();
+                button2.BackColor = Color.Green;
+            }
+
+            else
+            {
+                puan = puan - 5;
+                label6.Text = puan.ToString();
+                button2.BackColor = Color.Red;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label4.Text = button3.Text;
+            ButonSıfırla();
+            if(label3.Text==label4.Text)
+            {
+                puan = puan + 10;
+                label6.Text = puan.ToString();
+                button3.BackColor = Color.Green;
+            }
+
+            else
+            {
+                puan = puan - 5;
+                label6.Text = puan.ToString();
+                button3.BackColor = Color.Red;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label4.Text = button4.Text;
+            ButonSıfırla();
+            if (label3.Text == label4.Text)
+            {
+                puan = puan + 10;
+                label6.Text = puan.ToString();
+                button4.BackColor = Color.Green;
+            }
+
+            else
+            {
+                puan = puan - 5;
+                label6.Text = puan.ToString();
+                button4.BackColor = Color.Red;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            label4.Text = button5.Text;
+            ButonSıfırla();
+            if (label3.Text == label4.Text)
+            {
+                puan = puan + 10;
+                label6.Text = puan.ToString();
+                button5.BackColor = Color.Green;
+            }
+
+            else
+            {
+                puan = puan - 5;
+                label6.Text = puan.ToString();
+                button5.BackColor = Color.Red; 
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            button6.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sure--;
+            label7.Text = sure.ToString();
+
+            if(sure==0)
+            {
+                timer1.Stop();
+                MessageBox.Show("Süreniz doldu! Puanınız:" + label6.Text);
+                Application.Exit();
+            }
+        }
+    }
+}
